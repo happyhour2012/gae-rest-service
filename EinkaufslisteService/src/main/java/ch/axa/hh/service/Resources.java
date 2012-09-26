d4e54691b5f454a7a071ca49764f3fb7ccaca91f
Rémy Schumm <remy.schumm@axa.ch>
2012-09-26 11:24:12 +0200

    Jürgen wollte das kommentiert haben.

--- EinkaufslisteService/src/main/java/ch/axa/hh/service/Resources.java
+++ EinkaufslisteService/src/main/java/ch/axa/hh/service/Resources.java
@@ -5,14 +5,18 @@ import java.util.logging.Logger;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 
-
 public class Resources {
 
-   @Produces
-   public Logger produceLog(InjectionPoint injectionPoint) {
-      return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
-   }
-   
+	/**
+	 * Da hat die Doku gefehlt.
+	 * 
+	 * @param injectionPoint
+	 * @return
+	 */
+	@Produces
+	public Logger produceLog(InjectionPoint injectionPoint) {
+		return Logger.getLogger(injectionPoint.getMember().getDeclaringClass()
+				.getName());
+	}
 
-   
 }
